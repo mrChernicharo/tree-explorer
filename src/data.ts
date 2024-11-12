d9b65ecdd8e6af75fb7e6aa1b200d9cd235f9946
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-11-12 16:44:34 -0300

    a tree graph, just like i wanted

---
+++ src/data.ts
@@ -0,0 +1,129 @@
+const data = {
+  name: "orgs",
+  children: [
+    {
+      name: "Google",
+      children: [
+        {
+          name: "Tom",
+          children: [
+            {
+              name: "ChatGPT",
+              children: [
+                { name: "2024-11-10", value: 50 },
+                { name: "2024-11-09", value: 50 },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-03", value: 50 },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Sarah",
+          children: [
+            {
+              name: "Gemini",
+              children: [
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-05", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                { name: "2024-11-02", value: 50 },
+              ],
+            },
+            {
+              name: "Stable Difusion",
+              children: [
+                { name: "2024-11-10", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                { name: "2024-11-01", value: 50 },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Meta",
+      children: [
+        {
+          name: "Roy",
+          children: [
+            {
+              name: "ChatGPT",
+              children: [
+                { name: "2024-11-09", value: 50 },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-07", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                { name: "2024-11-09", value: 50 },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-07", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                {
+                  name: "Click me PLEASE!!!!",
+                  children: [
+                    {
+                      name: "more",
+                      children: [
+                        {
+                          name: "even more",
+                          children: [
+                            {
+                              name: "more yet...",
+                              children: [
+                                { name: "2024-11-08", value: 50 },
+                                { name: "2024-11-07", value: 50 },
+                                { name: "2024-11-06", value: 50 },
+                                { name: "2024-11-04", value: 50 },
+                                { name: "2024-11-03", value: 50 },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-07", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                { name: "2024-11-09", value: 50 },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-07", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+                { name: "2024-11-09", value: 50 },
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-07", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-04", value: 50 },
+                { name: "2024-11-03", value: 50 },
+              ],
+            },
+            {
+              name: "Gemini",
+              children: [
+                { name: "2024-11-08", value: 50 },
+                { name: "2024-11-06", value: 50 },
+                { name: "2024-11-05", value: 50 },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export { data };
